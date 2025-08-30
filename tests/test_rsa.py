30611750aa8636a04d67b702223a6a3a87765664
Ved Jaggi <ved@vedjaggi.com>
2025-08-30 16:15:05 +0800

    test: create rsa encryption test
    create test for creating and reading RSA keys

---
+++ tests/test_rsa.py
@@ -0,0 +1,20 @@
+import unittest
+from os import path
+import os
+from p2pchat.encryption.rsa_encryption import create_keys, read_keys
+
+class TestRSAEncryption(unittest.TestCase):
+    def test_generate_key_pair(self):
+        public_keys_file = path.abspath(path.join(os.getcwd(), "user_data", "keys", "public_keys.pem"))
+        private_key_file = path.abspath(path.join(os.getcwd(), "user_data", "keys", "private_key.pem"))
+        create_keys(public_keys_file, private_key_file)
+        self.assertTrue(os.path.exists(public_keys_file))
+        self.assertTrue(os.path.exists(private_key_file))
+
+    def test_read_keys(self):
+        public_keys_file = path.abspath(path.join(os.getcwd(), "user_data", "keys", "public_keys.pem"))
+        private_key_file = path.abspath(path.join(os.getcwd(), "user_data", "keys", "private_key.pem"))
+        create_keys(public_keys_file, private_key_file)
+        self.assertTrue(os.path.exists(public_keys_file))
+        self.assertTrue(os.path.exists(private_key_file))
+        self.assertIsNotNone(read_keys(public_keys_file, private_key_file))
