77e96a6fa2326c82a96baf1ae7096871277f894d
Ved Jaggi <ved@vedjaggi.com>
2025-11-07 12:41:05 +0800

    test: test for full message stack (with encryption)

---
+++ tests/test_message_stack.py
@@ -0,0 +1,19 @@
+import unittest
+
+from p2pchat.encryption.rsa_message_encrypt import (
+    create_message_wrapper,
+    decode_message_wrapper,
+)
+
+
+class TestAES(unittest.TestCase):
+    def test_encrypt_decrypt(self):
+        message = create_message_wrapper(
+            [
+                (b"goon", "txt"),
+            ],
+            b"ved",
+            "add_member",
+        )
+        decoded_message = decode_message_wrapper(message.json)
+        self.assertIsNotNone(decoded_message)
