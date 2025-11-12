09bdc70a872e1e1b0b2680e91b556ec8cafb243b
Ved Jaggi <ved@vedjaggi.com>
2025-11-12 21:11:27 +0800

    fix: takes into account '/' char in b64 encoding for fpaths
    very annoying (and rare) issue caused by the 0b111111 character in b64
    encoding to have a / character which unix OSes assume is a directory
    delimiter. solved by making an interface ontop of the b64 encode and
    decoder for slashes turning them into hyphens

--- p2pchat/response/responder.py
+++ p2pchat/response/responder.py
@@ -3,13 +3,19 @@ import os
 
 
 class Responder:
-    MESSAGE_GROUPS = {}
+    MESSAGE_GROUPS: dict[bytes, str] = {}
 
     def __getitem__(self, name: bytes, /) -> str:
         return self.MESSAGE_GROUPS[name]
 
+    def _filevalid_base64_encoded(self, name: bytes) -> str:
+        return base64.b64encode(name).decode("utf-8").replace("/", "_")
+
+    def _filevalid_base64_decode(self, name: str) -> bytes:
+        return base64.b64decode(name.replace("_", "/"))
+
     def create_message_group(self, group_unique_id: bytes) -> None:
-        base_64_id = base64.b64encode(group_unique_id).decode("utf-8")
+        base_64_id = self._filevalid_base64_encoded(group_unique_id)
         # Create a folder within userdata for the group
         user_data_folder = os.path.abspath(os.path.join(os.getcwd(), "user_data"))
         group_path = os.path.join(user_data_folder, base_64_id)
@@ -30,7 +36,7 @@ class Responder:
         self.MESSAGE_GROUPS[group_unique_id] = group_path
 
     def delete_local_group_message(self, group_unique_id: bytes) -> None:
-        base_64_id = base64.b64encode(group_unique_id).decode("utf-8")
+        base_64_id = self._filevalid_base64_encoded(group_unique_id)
         # Create a folder within userdata for the group
         user_data_folder = os.path.abspath(os.path.join(os.getcwd(), "user_data"))
         group_path = os.path.join(user_data_folder, base_64_id)
