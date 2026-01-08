3102a6f994c909c73161b0a8dd74e48fd63ceead
Ved Jaggi <ved@vedjaggi.com>
2026-01-08 22:50:25 +0800

    feat: improve generate_env.py to generate and run a complete venv
    improved generate_env.py to start all necessary worker threads, before
    shutting down after 1s. this will be improved on furthur in the future
    to allow for more effectiving testing of the protocol

--- generate_env.py
+++ generate_env.py
@@ -1,15 +1,54 @@
+import logging
 import queue
 import threading
+import time
 
-from p2pchat.encryption.rsa_encryption import get_rsa_key
+from p2pchat.packet.packet_communicator import PacketGateway
 from p2pchat.response.responder import Responder
 
-# Create RSA keys
-keys = get_rsa_key()
+logging.basicConfig(filename="user_data/main.log", filemode="w")
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
 
-# Create a test group
-shutdown = threading.Event()
-output: queue.Queue[bytes] = queue.Queue()
-responder = Responder(shutdown, output, None)
-group_id = b"TESTTESTTESTTEST"
-responder.create_message_group(group_id)
+
+def main():
+    # Shutdown event
+    shutdown = threading.Event()
+
+    # Create a response queue
+    output: queue.Queue[bytes] = queue.Queue()
+    comm = PacketGateway(shutdown)
+    responder = Responder(shutdown, output, comm)
+
+    # Create a receiver thread
+    receiver_thread = threading.Thread(target=incoming, args=(comm, shutdown, output))
+    receiver_thread.start()
+
+    time.sleep(1)
+    logger.debug("Complete, waiting for threads to finish...")
+    shutdown.set()
+    logger.debug("Shutdown set")
+    comm.receiver_thread.join()
+    logger.debug("Comm Thread joint")
+    responder.responder_thread.join()
+    logger.debug("Responder Thread joint")
+    receiver_thread.join()
+    logger.debug("Receiver Thread joint")
+    comm.close_socks()
+    logger.debug("Comm Sockets closed")
+    logger.debug("Point precision!")
+
+
+def incoming(
+    communicator: PacketGateway, shutdown: threading.Event, output: queue.Queue[bytes]
+):
+    while not shutdown.is_set():
+        try:
+            msg = communicator.reassembled_messages.get(timeout=0.5)
+            output.put(msg)
+        except queue.Empty:
+            continue
+
+
+if __name__ == "__main__":
+    main()
